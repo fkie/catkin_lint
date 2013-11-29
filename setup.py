d016a83578fb205b75718227c9d14b22148f3ce6
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2013-11-29 09:31:50 +0100

    1.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
   packages      = [],
   package_dir   = {},
   scripts       = [ "catkin_lint" ],
-  version       = "1.0.4",
+  version       = "1.0.5",
   requires      = [ "catkin_pkg" ]
 )
 
