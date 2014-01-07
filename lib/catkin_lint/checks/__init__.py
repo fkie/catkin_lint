aec6e1913ce77c06bf9b477fbc732a37676fb5aa
Timo Röhling <timo.roehling@gmx.de>
2014-01-07 15:16:13 +0100

    CMake lint checks
    These checks verify that the project name matches the package name,
    certain special variables are not tampered with, and certain commands
    are not invoked more than once.

--- lib/catkin_lint/checks/__init__.py
+++ lib/catkin_lint/checks/__init__.py
@@ -5,5 +5,8 @@ __author__ = "Timo Röhling <timo.roehling@fkie.fraunhofer.de>"
 __copyright__ = "Copyright (c) 2013 Fraunhofer FKIE"
 __license__ = "BSD"
 
+import cmake
 def add_all_checks(linter):
-    pass
+    cmake.project(linter)
+    cmake.special_vars(linter)
+    cmake.singleton_commands(linter)
