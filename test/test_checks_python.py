a064b907bcb387af38027473594313ad067b544b
Timo Röhling <timo.roehling@gmx.de>
2014-01-10 00:48:31 +0100

    Add unit tests for python checks

---
+++ test/test_checks_python.py
@@ -0,0 +1,36 @@
+import unittest
+import catkin_lint.checks.python as cc
+from .helper import create_env, create_manifest, mock_lint
+
+import sys
+from unittest.mock import patch
+
+sys.stderr = sys.stdout
+
+
+class ChecksPythonTest(unittest.TestCase):
+
+    @patch("os.path.exists", new=lambda x: False)
+    def test_setup_without_setup_py(self):
+        env = create_env()
+        pkg = create_manifest("mock")
+
+        result = mock_lint(env, pkg, "", checks=cc.setup)
+        self.assertEqual([], result)
+
+        result = mock_lint(env, pkg, "project(mock) find_package(catkin REQUIRED) catkin_python_setup()", checks=cc.setup)
+        self.assertEqual([ "MISSING_FILE" ], result)
+
+    @patch("os.path.exists", new=lambda x: True)
+    def test_setup_with_setup_py(self):
+        env = create_env()
+        pkg = create_manifest("mock")
+
+        result = mock_lint(env, pkg, "project(mock) find_package(catkin REQUIRED) catkin_python_setup()", checks=cc.setup)
+        self.assertEqual([], result)
+
+        result = mock_lint(env, pkg, "project(mock) catkin_python_setup()", checks=cc.setup)
+        self.assertEqual([ "CATKIN_ORDER_VIOLATION" ], result)
+
+        result = mock_lint(env, pkg, "project(mock)", checks=cc.setup)
+        self.assertEqual([ "MISSING_PYTHON_SETUP" ], result)
