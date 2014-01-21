4522ff76f8f7458e21d4031f8b32b9f86fde46e3
Timo Röhling <timo.roehling@gmx.de>
2014-01-22 00:44:31 +0100

    Script for PPA upload

---
+++ debian/extra/update-ppa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+sa="-sa"
+./debian/rules make-orig-tar
+for distro in precise quantal raring saucy trusty
+do
+    sed -i -e '1s/\(~[a-z]\+\)\?) [a-z]\+;/~'$distro') '$distro';/' debian/changelog
+    dpkg-buildpackage -S $sa -i\\..*
+    version="$( dpkg-parsechangelog | grep Version | cut -d' ' -f2 )"
+    dput ppa:roehling/latest ../catkin-lint_${version}_source.changes
+    sa="-sd"
+done
+git checkout -- debian/changelog
+
