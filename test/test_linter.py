e6f43a81c750573be933419793166303d2f8640c
Timo Röhling <timo.roehling@gmx.de>
2014-01-22 01:40:12 +0100

    Additional unit tests for include() and add_subdirectory()

--- test/test_linter.py
+++ test/test_linter.py
@@ -21,8 +21,20 @@ class LinterTest(unittest.TestCase):
         linter = CMakeLinter(create_env())
         self.assertRaises(RuntimeError, linter.require, a)
 
+    @patch("os.path.isfile", lambda x: x == "/mock-path/broken.cmake")
+    def test_blacklist(self):
+        env = create_env()
+        pkg = create_manifest("catkin")
+        result = mock_lint(env, pkg,
+            {
+               "/mock-path/CMakeLists.txt": "project(catkin) include(broken.cmake RESULT_VARIABLE gone) catkin_package()",
+               "/mock-path/broken.cmake": "xxxxxx syntax error xxxxx"
+            }, checks=cc.all
+        )
+        self.assertEqual([], result)
+
     @patch("os.path.isdir", lambda x: x in [ "/mock-path/src", "/mock-path/include" ])
-    @patch("os.path.isfile", lambda x: x in  [ "/mock-path/src/CMakeLists.txt", "/mock-path/src/mock.cpp" ])
+    @patch("os.path.isfile", lambda x: x in  [ "/other-path/CMakeLists.txt", "/mock-path/src/CMakeLists.txt", "/mock-path/src/mock.cpp" ])
     def test_subdir(self):
         env = create_env()
         pkg = create_manifest("mock")
@@ -52,6 +64,27 @@ class LinterTest(unittest.TestCase):
             }, checks=cc.all
         )
         self.assertEqual([ "DUPLICATE_SUBDIR" ], result)
+
+        result = mock_lint(env, pkg,
+            {
+              "/mock-path/CMakeLists.txt" : "project(mock) add_subdirectory(/other-path)",
+              "/other-path/CMakeLists.txt" : """
+              find_package(catkin REQUIRED)
+              catkin_package()
+              """
+            }, checks=cc.all
+        )
+        self.assertEqual([ "EXTERNAL_SUBDIR" ], result)
+
+        result = mock_lint(env, pkg,
+              """
+              project(mock)
+              find_package(catkin REQUIRED)
+              catkin_package()
+              add_subdirectory(missing_subdir)
+              """, checks=cc.all
+        )
+        self.assertEqual([ "MISSING_SUBDIR" ], result)
 
         result = mock_lint(env, pkg,
             {
