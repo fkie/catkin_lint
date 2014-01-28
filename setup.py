e6c3d7f0d9393af695844200f8ca3556049b2cbc
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2014-01-28 15:15:49 +0100

    Make sys.path hackery more robust
    This allows run.py to be invoked from a different cwd than the package
    source folder.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 import sys
 import os
-sys.path.insert(0, "src")
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 from catkin_lint import __version__ as catkin_lint_version
 
 def read(fname):
