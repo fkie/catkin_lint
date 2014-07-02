fb8e9a8b9d026db494b8115659b15e6951ba131c
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2014-07-02 09:37:03 +0200

    1.3.7

--- src/catkin_lint/__init__.py
+++ src/catkin_lint/__init__.py
@@ -4,6 +4,6 @@
 __author__ = "Timo Röhling <timo.roehling@fkie.fraunhofer.de>"
 __copyright__ = "Copyright (c) 2013,2014 Fraunhofer FKIE"
 __license__ = "BSD"
-__version__ = "1.3.6"
+__version__ = "1.3.7"
 __all__ = [ "cmake", "diagnostics", "linter", "main", "output", "packages", "util" ]
 
