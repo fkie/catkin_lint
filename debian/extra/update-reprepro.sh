d405575c5e441a8a745ffb22e6188966a9dd7a2f
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2014-12-16 14:11:53 +0100

    Update build scripts

--- debian/extra/update-reprepro.sh
+++ debian/extra/update-reprepro.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 set -e
-pkgname=$( dpkg-parsechangelog | grep ^Source: | cut -d' ' -f2 )
-pkgversion=$( dpkg-parsechangelog | grep ^Version: | cut -d' ' -f2 )
 build_type=$(<debian/source/format)
+sa="-sa"
 if [ "$build_type" = "3.0 (quilt)" ]
 then
 	debian/rules make-orig-tar
 fi
-dpkg-buildpackage -tc -us -uc -i"\\..*" "$@"
-changefile=$( readlink -f ../${pkgname}_${pkgversion}_*.changes )
-buildfiles=( $( grep -A999 Files: "${changefile}" | awk '{ print $5 }' ) )
-
-cleanup()
-{
-  cd $(dirname "${changefile}" )
-  rm "${buildfiles[@]}" "${changefile}"
-}
-
-trap cleanup EXIT
-
-cd $HOME/reprepro
-for dist in precise quantal raring saucy trusty
+export BUILD_ARCH=amd64
+export DPKG_BUILDPACKAGE_OPTS="-i\\..*"
+for distro in precise trusty
 do
-	reprepro include ${dist}-fkie ${changefile}
+	sed -i -e '1s/\(~[a-z]\+\)\?) [a-z]\+;/~'$distro') '$distro';/' debian/changelog
+	../builddeb.sh "$sa"
+	sa="-sd"
 done
+git checkout -- debian/changelog
 
