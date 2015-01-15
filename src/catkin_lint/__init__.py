974f429209ec1aef696f6ff87680e73af14a1533
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2015-01-15 13:29:50 +0100

    Modified BSD license
    This commit adds the 3rd clause prohibiting unauthorized endorsement
    and promotion using the name Fraunhofer or the software's contributors.

--- src/catkin_lint/__init__.py
+++ src/catkin_lint/__init__.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 __author__ = "Timo Röhling <timo.roehling@fkie.fraunhofer.de>"
-__copyright__ = "Copyright (c) 2013,2014 Fraunhofer FKIE"
+__copyright__ = "Copyright (c) 2013-2015 Fraunhofer FKIE"
 __license__ = "BSD"
 __version__ = "1.3.11"
 __all__ = [ "cmake", "diagnostics", "linter", "main", "output", "packages", "util" ]
--- src/catkin_lint/checks/manifest.py
+++ src/catkin_lint/checks/manifest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Copyright (c) 2013,2014 Fraunhofer FKIE
+Copyright (c) 2013-2015 Fraunhofer FKIE
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
@@ -11,6 +11,9 @@ are met:
  * Redistributions in binary form must reproduce the above copyright
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
+ * Neither the name of the Fraunhofer organization nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
 IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
