7167d4180a02185cb63f864d0b4a8ca5a60fdb3f
Timo Röhling <timo.roehling@gmx.de>
2015-12-12 23:57:51 +0100

    Update Ubuntu version info

--- debian/extra/update-ppa.sh
+++ debian/extra/update-ppa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-declare -A ubuntu=( [precise]=12.04 [trusty]=14.04 [utopic]=14.10 [vivid]=15.04 [wily]=15.10 )
+declare -A ubuntu=( [precise]=12.04 [trusty]=14.04 [utopic]=14.10 [vivid]=15.04 [wily]=15.10 [xenial]=16.04 )
 sa="-sa"
 ./debian/rules make-orig-tar
-for distro in precise trusty utopic vivid
+for distro in precise trusty vivid wily
 do
-    sed -i -e '1s/\(~.\+\)\?) [a-z]\+;/~ubuntu'${ubuntu[$distro]}') '$distro';/' debian/changelog
+    sed -i -e '1s/\(~.\+\)\?) [a-z]\+;/~'${ubuntu[$distro]}') '$distro';/' debian/changelog
     dpkg-buildpackage -S $sa -i\\..*
     version="$( dpkg-parsechangelog | grep Version | cut -d' ' -f2 )"
     dput ppa:roehling/latest ../catkin-lint_${version}_source.changes
