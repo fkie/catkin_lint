a89f9097380c5184e397c4d68afc6d41950491aa
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2021-08-01 15:18:26 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     version=versioneer.get_version(),
     cmdclass=cmdclass,
     install_requires=["catkin_pkg", "lxml", "configparser<5;python_version<\"3\""],
-    extras_reqire={
+    extras_require={
         "ros": ["rosdistro", "rosdep"],
     },
     tests_require=["nose", "coverage", "mock"],
